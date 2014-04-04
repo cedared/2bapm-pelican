4f6e036abaae70d2034125afff12fbf7a11862ad
Albert Young <yangsong@2bapm.com>
2014-04-04 23:41:14 +0800

    Init Commit
    第一次提交，包含了部分修改过的主题，以及修改过的makefile和pelicanconf

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Albert Young'
+SITENAME = u'Just Be Free!'
+SITEURL = ''
+SITESUBTITLE = u'Freedom,Foolish,Simple!'
+PERSONAL_INFO = u'My name is Albert Young, a Product Manager who is working & living in Shenzhen, China. I\'ve spent about 4 years in internet industry from doing the operation of the Internet at first, to work as a Product Manager  in one of the greatest company in this world. I also do program work a lot as a personal hobby in my part-time. I learnt variety of languages, like C, C++, ASM, Object-Pascal since 6th grade in elementary school. But Python has been my favorite since I knew it for the first time in 2006. Also, English is my second language while my mother tongue is Madrin. I can speak some Japanese, and now is preparing for the official Japanese certification.'
+
+TIMEZONE = 'Aisa/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+THEME = "/Users/albert/kuaipan/Dropbox/blog/theme/BT3-Flat"
+
+DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives', 'blog')
+PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
+POST_LIMIT=10
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('艺术壮士', 'http://yishuzs.com/'),
+          ('network tsukkomi', 'http://networktsukkomi.me/'),)
+
+# Social widget
+SOCIAL = (('envelope', 'mailto:yangsong@2bapm.com'),
+          ('twitter', 'http://twitter.com/AlbertYoung1'),
+          ('github', 'https://github.com/cedared'),
+          ('google-plus', 'https://plus.google.com/+AlbertYoungcedared/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
